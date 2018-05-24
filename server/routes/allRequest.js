de118cef1932501c07ba781009c905cf36864256
Affiong Uno <unoaffiong>
2018-05-23 20:00:43 -1200

    created route to fetch request by id

--- server/routes/allRequest.js
+++ server/routes/allRequest.js
@@ -5,5 +5,7 @@ const requestRoute = express.Router();
 
 requestRoute.get('/', requestController.getRequest);
 requestRoute.post('/', requestController.createRequest); 
+requestRoute.get('/:id', requestController.getRequestById);
+requestRoute.put('/:id', requestController.modifyRequestById);
 
 export default requestRoute;
